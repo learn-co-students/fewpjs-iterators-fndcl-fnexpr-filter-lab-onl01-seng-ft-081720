ad21492c40915d224499df98dcab16de2776a479
Eli Cusic <tntitans31@gmail.com>
2020-11-26 10:38:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(arr, name){
+    let newArr = arr.filter(function(x){
+        return x.toUpperCase() === name.toUpperCase()
+    })    
+    return newArr
+}
+
+function fuzzyMatch(arr, str){
+    let newArr = arr.filter(function(x){
+        return x.slice(0, str.length).toUpperCase() === str.toUpperCase()
+    })
+    return newArr
+}
+
+function matchName(arr, str){
+    let newArr = arr.filter(function(x){
+        return x.name.slice(0, str.length).toUpperCase() === str.toUpperCase()
+    })  
+    return newArr
+}
