c7cf99e6cc7528147a2660c5e37f03fa0a0b3888
ellenupark <ellenupark@gmail.com>
2020-11-25 20:56:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(function (driver) {
+        return driver.toLowerCase() === str.toLowerCase();
+    });
+};
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(function(driver) {
+        return driver.split("").splice(0, str.length).join("") === str;
+    });
+}
+
+function matchName(arr, str) {
+    return arr.filter(function(driverObj) {
+        return driverObj["name"] == str;
+    });
+}
