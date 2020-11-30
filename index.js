948a69d0f4fc108a68cc42df6be52bf90445247f
devWRM <mcwen@comcast.net>
2020-11-30 02:30:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+
+function findMatching(driversArray, driverString) {
+    return driversArray.filter( driver =>  {
+        return driver.toLowerCase() === driverString.toLowerCase()
+    })
+}
+
+
+// RESOURCE: https://www.youtube.com/watch?v=eqPYvrxLm4c
+        // indexOf()      -vs-        lastIndexOf()
+function fuzzyMatch(driversArray, lettersString) {
+    return driversArray.filter( driver => {
+        return driver.toLowerCase().indexOf(lettersString.toLowerCase()) === 0
+})
+}
+
+
+function matchName(driversObjArray, driverString) {
+    return driversObjArray.filter( driver => {
+        return driver["name"] === driverString
+    })
+}
