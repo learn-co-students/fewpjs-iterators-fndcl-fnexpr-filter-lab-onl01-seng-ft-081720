96f828d74fafb4adfc9070095e978a02c47bf3fe
SophiaGrace16 <sophia.g.white@outlook.com>
2020-12-01 10:21:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(function (driver) {
+        return driver.toLowerCase() === str.toLowerCase();
+    });
+};
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(function(driver) {
+        return driver.slice(0, str.length).toUpperCase() === str.toUpperCase();
+    });
+}
+
+function matchName(arr, str) {
+    return arr.filter(function(driverObj) {
+        return driverObj["name"] == str;
+    });
+} 
