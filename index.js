718e3c8b3765e6998bb2e208eb87f0ddfeaefc65
Newland-A <ajn252@gmail.com>
2020-12-02 19:55:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+// let arr = [1, 2, 3, 4, 5, 6];
+// let even = [];
+// think of using .filter as using a for loop for(var i = 0; i < array.length; i++){
+  // if (array[i] % 2 === 0) even.push(array[i]);
+// }
+
+// turns in this when using the .filter method
+// let even = array.filter(n => {
+//   return n % 2  === 0;
+// });
+
+function findMatching(drivers, string) {
+  return drivers.filter(drivers => drivers.toLowerCase().indexOf(string.toLowerCase()) === 0 
+  )
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(drivers => drivers.toLowerCase().indexOf(string.toLowerCase()) === 0 
+  )
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(drivers => drivers.name === string )
+}
