00f4a9d47bea148a63dce6fc87b3b68fbdcf84ee
Bri8102 <damioniru16@gmail.com>
2020-12-10 15:01:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers,name) {
+   let matching =  drivers.filter(function(n){
+      return n.toUpperCase() === name.toUpperCase()
+   })
+   return matching
+}
+
+function fuzzyMatch(drivers,name) {
+    let fuzzy = drivers.filter(function(n) {
+        return n.slice(0,name.length).toUpperCase() === name.toUpperCase()
+    })
+    return fuzzy
+}
+
+function matchName(drivers,name) {
+    let match = drivers.filter(function(n){
+      return n.name.slice(0,name.length).toUpperCase() === name.toUpperCase()
+    })
+    return match
+}
